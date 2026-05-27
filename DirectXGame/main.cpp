eeb4de21173d40ad0026260c22636352812e702b
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-05-27 12:26:15 +0900

    CG5_00_02

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -165,7 +165,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//頂点リソースのマップを解除
 	vertexResource->Unmap(0, nullptr);
 
-
 	// メインループ
 	while (true) {
 		// エンジンの更新
