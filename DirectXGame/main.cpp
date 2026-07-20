2da6c2860711efb05701fa76008e61d200f2d49a
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-07-20 11:52:36 +0900

    dekita

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -21,7 +21,7 @@ void SetupPipeLineState(PipelineState& pipelineState, RootSignature& rs, Shader&
 	// POSITION
 	inputElementDescs[0].SemanticName = "POSITION";
 	inputElementDescs[0].SemanticIndex = 0;
-	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
 
 	// TEXCOORD
